994750dcd12fb798eb330dd806906751b470ae29
Rocket Primm <117668454+RPrimm@users.noreply.github.com>
2023-12-06 17:18:29 -0500

    inital

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+
+import Home from "./pages/Home";
+import ClinicianDashboard from "./pages/Clinician";
+import DesignerDashboard from "./pages/Designer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact Component={Home} />
+          <Route path="/clinician" exact Component={ClinicianDashboard} />
+          <Route path="/designer" exact Component={DesignerDashboard} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Designer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Designer() {
+  return (
+    <div>Designer</div>
+  )
+}
+
+export default Designer
